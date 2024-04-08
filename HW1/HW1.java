a3c44647c3a31ded6b41b5c21a1aa60927e47fac
Quang10A3ChuyenBo <88416001+Quang10A3ChuyenBo@users.noreply.github.com>
2024-04-08 22:31:02 +0700

    Add files via upload

---
+++ HW1/HW1.java
@@ -0,0 +1,14 @@
+public class Profile {
+     public static void main(String[] args){
+         System.out.print("Họ và tên: Võ Duy Quang\t");
+         System.out.print("MSV: 23020414\t");
+         System.out.print("Lớp: K68-AI2\t");
+         System.out.print("Username: Quang10A3ChuyenBo\t");
+         System.out.println("Địa chỉ email: vdqg24@gmail.com");
+         for(int i=9; i>=1; i--){
+             System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+             System.out.println("Take one down, pass it around,");
+         }
+         System.out.println("No more bottles of beer on the wall.");
+     }
+}
